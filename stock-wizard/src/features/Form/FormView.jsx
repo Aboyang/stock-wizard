77694a17484794f4ed2ca8b8a85ef1c914fed0b7
Tee Kai Yang <tkaiyang2005@gmail.com>
2025-06-30 15:27:58 +0800

    Add files via upload

---
+++ stock-wizard/src/features/Form/FormView.jsx
@@ -0,0 +1,123 @@
+import { useSelector, useDispatch } from "react-redux"
+import { addSymbol, fetchData, clearSecs } from "../securitySlice"
+import { changeInterval, changeStart, changeEnd } from "../Form/formSlice"
+import { useEffect, useState, useRef } from "react"
+import axios from "axios"
+
+import './FormView.css'
+
+function FormView() {
+
+    // for the search bar
+    const [symbol, setSymbol] = useState('')
+    const [suggestion, setSuggestion] = useState([])
+
+    // redux
+    const { symbols } = useSelector((state) => state.security)
+    const { start, end, interval } = useSelector((state) => state.form)
+    const dispatch = useDispatch()
+
+    const run = useRef(false)
+
+    // handle onclick for adding stock
+    function handleAddSec(symbol) {
+        if (symbols.includes(symbol)) return // prevent repetition
+
+        dispatch(addSymbol(symbol))
+        dispatch(fetchData(symbol))
+        
+        setSymbol('')
+    }
+
+    // every data needa be re-fetched whenever one of the parameters (interval, start, end) changes
+    useEffect(() => {
+
+        if (run.current) return
+
+        dispatch(clearSecs())
+
+        for (let symbol of symbols) {
+
+            // make sure the data of the current symbol is fetched and stored before proceeding to fetch the next
+            async function fetchBySymbol(){
+                await dispatch(fetchData(symbol))  
+            }
+
+            fetchBySymbol(symbol)
+        }
+
+        run.current = true
+
+    }, [start, end, interval])
+
+
+    // search bar
+    useEffect(() => {
+
+        if (symbol === "") {
+
+            if (suggestion.length === 0) {
+                return
+            } 
+
+            setSuggestion([])
+            return
+        }
+
+        axios.get(`http://localhost:5001/api/suggestion?search=${symbol}`).then(response => setSuggestion(response.data))
+
+    }, [symbol])
+
+    
+
+
+
+    return (
+        <div className="setting">
+
+            <div className="symbol-container">
+                <input className="text" type="text" placeholder="TSLA" value={symbol} onChange={(e) => setSymbol(e.target.value)}/>
+                <div className="btn" onClick={() => handleAddSec(symbol)}><i className="icon fa-solid fa-magnifying-glass"></i></div>
+            </div>
+
+            {suggestion.length > 0 && (
+                <div className="suggestions">
+                    {suggestion.map((symb, index) => (<div className="suggestion" key={index} onClick={() => {
+
+                        handleAddSec(symb)
+                        setSymbol('')
+                        setSuggestion([])
+                        
+                    }}>{symb}
+
+                </div>
+                    ))}
+                </div>
+            )}
+
+            <div className="date-container">
+                <input className="date" type="date" value={start} onChange={(e) => {
+                    dispatch(changeStart(e.target.value))
+                    run.current = false
+                }} />
+                to
+                <input className="date" type="date" value={end} onChange={(e) => {
+                    dispatch(changeEnd(e.target.value))
+                    run.current = false
+                }} />
+            </div>
+            
+            <select className="menu" value={interval || ""} onChange={(e) => {
+                dispatch(changeInterval(e.target.value))
+                run.current = false
+            }}>
+                <option value="1d">1 Day</option>
+                <option value="1wk">1 Week</option>
+                <option value="1mo">1 Month</option>
+            </select>
+
+        </div>
+    )
+}
+
+export default FormView
